79c825fa649aace27ce063818d6eea1efa9edb98
chneez <100981013+chneez@users.noreply.github.com>
2024-02-12 12:39:51 +0800

    更新 wodde_token.js

---
+++ js/wodde_token.js
@@ -0,0 +1,28 @@
+const $ = new Env('xxqatoken')
+
+!(async () => {
+  $.log('', `🔔 ${$.name}, 获取会话: 开始!`, '')
+  
+
+
+const token = $request.headers["access-token"];
+
+  //$.log(`${$.name}`, `body: ${JSON.stringify($request.body)}`, '')
+
+$.setjson(token, 'xxqa_token')
+
+
+  $.subt = '获取会话: 成功!'
+})()
+  .catch((e) => {
+    $.subt = '获取会话: 失败!'
+    $.desc = `原因: ${e}`
+    $.log(`❌ ${$.name}, 获取会话: 失败! 原因: ${e}!`)
+  })
+  .finally(() => {
+    $.msg($.name, $.subt, $.desc), $.log('', `🔔 ${$.name}, 获取会话: 结束!`, ''), $.done()
+  })
+
+
+
+
